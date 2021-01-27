1e0725f71e603ea71bbcb9b1f17bbc1437267a10
user <rstencel@soldevelo.com>
2021-01-27 17:02:48 +0100

    OFS-165: Contracts search page

--- README.md
+++ README.md
@@ -1,1 +1,104 @@
-# openimis-fe-contract_js
+# openIMIS Frontend Contract module
+This repository holds the files of the openIMIS Frontend Contract reference module.
+It is dedicated to be deployed as a module of [openimis-fe_js](https://github.com/openimis/openimis-fe_js).
+
+[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)
+
+## Main Menu Contributions
+None
+
+## Other Contributions
+* `core.Router`: registering `contracts` route in openIMIS client-side router
+* `insuree.MainMenu`:
+    
+    **Contracts** (`menu.contracts` translation key)
+
+## Available Contribution Points
+None
+
+## Published Components
+None
+
+## Dispatched Redux Actions
+* `CONTRACT_CONTRACTS_{REQ|RESP|ERR}`, fetching Contracts (as triggered by the searcher)
+* `CONTRACT_MUTATION_{REQ|ERR}`, sending a mutation
+
+## Other Modules Listened Redux Actions
+None
+
+## Other Modules Redux State Bindings
+* `state.core.user`, to access user info (rights,...)
+* `state.policyHolder`, retrieving Policy Holders for Policy Holder picker used for filtering Contracts
+
+## Configurations Options
+* `contractFilter.contractStateOptions`: options for ContractStatePicker component (Default:
+    ```json
+    [{
+        "value": "1",
+        "label": {
+            "fr": "Demande d'information",
+            "en": "Request for information"
+        }
+    }, {
+        "value": "2",
+        "label": {
+            "fr": "Brouillon",
+            "en": "Draft"
+        }
+    }, {
+        "value": "3",
+        "label": {
+            "fr": "Offre",
+            "en": "Offer"
+        }
+    }, {
+        "value": "4",
+        "label": {
+            "fr": "En negociation",
+            "en": "Negotiable"
+        }
+    }, {
+        "value": "5",
+        "label": {
+            "fr": "Apprové",
+            "en": "Executable"
+        }
+    }, {
+        "value": "6",
+        "label": {
+            "fr": "Addendum",
+            "en": "Addendum"
+        }
+    }, {
+        "value": "7",
+        "label": {
+            "fr": "En cours",
+            "en": "Effective"
+        }
+    }, {
+        "value": "8",
+        "label": {
+            "fr": "Appliqué",
+            "en": "Executed"
+        }
+    }, {
+        "value": "9",
+        "label": {
+            "fr": "Suspendu",
+            "en": "Disputed"
+        }
+    }, {
+        "value": "10",
+        "label": {
+            "fr": "Terminé",
+            "en": "Terminated"
+        }
+    }, {
+        "value": "11",
+        "label": {
+            "fr": "Révision demandé",
+            "en": "Counter"
+        }
+    }]
+    ```
+)
---
+++ src/components/ContractSearcher.js
@@ -0,0 +1,124 @@
+import React, { Component, Fragment } from "react"
+import { injectIntl } from 'react-intl';
+import { withModulesManager, formatMessageWithValues, Searcher, formatDateFromISO } from "@openimis/fe-core";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import { fetchContracts } from "../actions"
+import { DEFAULT_PAGE_SIZE, ROWS_PER_PAGE_OPTIONS } from "../constants"
+import ContractFilter from "./ContractFilter";
+import ContractStatePicker from "../pickers/ContractStatePicker";
+
+class ContractSearcher extends Component {
+    fetch = params => this.props.fetchContracts(this.props.modulesManager, params);
+
+    filtersToQueryParams = state => {
+        let params = Object.keys(state.filters)
+            .filter(f => !!state.filters[f]['filter'])
+            .map(f => state.filters[f]['filter']);
+        params.push(`first: ${state.pageSize}`);
+        if (!state.filters.hasOwnProperty('isDeleted')) {
+            params.push("isDeleted: false");
+        }
+        if (!!state.afterCursor) {
+            params.push(`after: "${state.afterCursor}"`);
+        }
+        if (!!state.beforeCursor) {
+            params.push(`before: "${state.beforeCursor}"`);
+        }
+        if (!!state.orderBy) {
+            params.push(`orderBy: ["${state.orderBy}"]`);
+        }
+        return params;
+    }
+
+    headers = () => [
+        "contract.code",
+        "contract.state",
+        "contract.amount",
+        "contract.datePaymentDue",
+        "contract.dateValidFrom",
+        "contract.dateValidTo",
+        "contract.amendment"
+    ]
+
+    itemFormatters = () => {
+        const { intl, modulesManager } = this.props;
+        let result = [
+            contract => !!contract.code ? contract.code : "",
+            contract => !!contract.state
+                ? <ContractStatePicker
+                    value={contract.state}
+                    readOnly />
+                : "",
+            contract => contract.amountDue !== null
+                ? contract.amountDue
+                : contract.amountRectified !== null
+                    ? contract.amountRectified
+                    : contract.amountNotified !== null
+                        ? contract.amountNotified
+                        : "",
+            contract => !!contract.datePaymentDue
+                ? formatDateFromISO(modulesManager, intl, contract.datePaymentDue)
+                : "",
+            contract => !!contract.dateValidFrom
+                ? formatDateFromISO(modulesManager, intl, contract.dateValidFrom)
+                : "",
+            contract => !!contract.dateValidTo
+                ? formatDateFromISO(modulesManager, intl, contract.dateValidTo)
+                : "",
+            contract => contract.amendment !== null ? contract.amendment : "",
+        ];
+        return result;
+    }
+
+    sorts = () => [
+        ['code', true],
+        ['state', true],
+        null,
+        ['datePaymentDue', true],
+        ['dateValidFrom', true],
+        ['dateValidTo', true],
+        ['amendment', true],
+    ];
+
+    render() {
+        const { intl, fetchingContracts, fetchedContracts, errorContracts, contracts, contractsPageInfo, contractsTotalCount } = this.props;
+        return (
+            <Fragment>
+                <Searcher
+                    module="contact"
+                    FilterPane={ContractFilter}
+                    fetch={this.fetch}
+                    items={contracts}
+                    itemsPageInfo={contractsPageInfo}
+                    fetchingItems={fetchingContracts}
+                    fetchedItems={fetchedContracts}
+                    errorItems={errorContracts}
+                    tableTitle={formatMessageWithValues(intl, "contract", "contracts.searcher.results.title", { contractsTotalCount })}
+                    headers={this.headers}
+                    itemFormatters={this.itemFormatters}
+                    filtersToQueryParams={this.filtersToQueryParams}
+                    sorts={this.sorts}
+                    rowsPerPageOptions={ROWS_PER_PAGE_OPTIONS}
+                    defaultPageSize={DEFAULT_PAGE_SIZE}
+                    defaultOrderBy="code"
+                />
+            </Fragment>
+        )
+    }
+}
+
+const mapStateToProps = state => ({
+    fetchingContracts: state.contract.fetchingContracts,
+    fetchedContracts: state.contract.fetchedContracts,
+    errorContracts: state.contract.errorContracts,
+    contracts: state.contract.contracts,
+    contractsPageInfo: state.contract.contractsPageInfo,
+    contractsTotalCount: state.contract.contractsTotalCount
+});
+
+const mapDispatchToProps = dispatch => {
+    return bindActionCreators({ fetchContracts }, dispatch);
+};
+
+export default withModulesManager(injectIntl(connect(mapStateToProps, mapDispatchToProps)(ContractSearcher)));
