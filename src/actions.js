c712aefb8ca117fd9de030c397dfe7bd766feff3
sniedzielski <sniedzielski@soldevelo.com>
2022-04-26 14:46:19 +0200

    OP-759: fixed creating contract from PolicyHolder page

--- src/actions.js
+++ src/actions.js
@@ -3,6 +3,8 @@ import {
 } from "@openimis/fe-core";
 import { APPLY_DEFAULT_VALIDITY_FILTER } from "./constants";
 
+const regexExp = /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi;
+
 const CONTRACT_FULL_PROJECTION = modulesManager => [
     "id", "code", "amount", "amountNotified", "amountRectified", "amountDue", "dateApproved", "datePaymentDue",
     "state", "paymentReference", "amendment", "dateValidFrom", "dateValidTo", "isDeleted",
@@ -94,10 +96,16 @@ export function fetchInsureePolicies(modulesManager, params) {
 }
 
 function formatContractGQL(contract, readOnlyFields = []) {
+    // check if uuid decoded correctly 
+    var policyHolderId = null;
+    if (contract.policyHolder && !readOnlyFields.includes('policyHolder')){
+        policyHolderId = contract.policyHolder.id 
+        policyHolderId = (regexExp.test(policyHolderId))? policyHolderId : decodeId(policyHolderId);
+    }
     return `
         ${!!contract.id ? `id: "${decodeId(contract.id)}"` : ''}
         ${!!contract.code && !readOnlyFields.includes('code') ? `code: "${formatGQLString(contract.code)}"` : ""}
-        ${!!contract.policyHolder && !readOnlyFields.includes('policyHolder') ? `policyHolderId: "${contract.policyHolder.id}"` : ""}
+        ${!!contract.policyHolder && !readOnlyFields.includes('policyHolder') ? `policyHolderId: "${policyHolderId}"` : ""}
         ${!!contract.dateApproved && !readOnlyFields.includes('dateApproved') ? `dateApproved: "${contract.dateApproved}"` : ""}
         ${!!contract.datePaymentDue && !readOnlyFields.includes('datePaymentDue') ? `datePaymentDue: "${contract.datePaymentDue}"` : ""}
         ${!!contract.paymentReference && !readOnlyFields.includes('paymentReference') ? `paymentReference: "${formatGQLString(contract.paymentReference)}"` : ""}
