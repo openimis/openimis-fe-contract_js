31f0448c66a0e7733ccdb0016f6b0407cd2c0340
olewandowski1 <109145288+olewandowski1@users.noreply.github.com>
2023-03-23 12:51:04 +0100

    OTC-757: Clearing pagination state if entered the ContractsPage (#51)
    * OTC-757: Code refactor in ContractsPage
    
    * OTC-757: Clearing pagination state if entered the ContractsPage
    
    * OTC-757: Hardcoded values moved to the constants

--- src/pages/ContractsPage.js
+++ src/pages/ContractsPage.js
@@ -1,58 +1,117 @@
 import React, { Component } from "react";
-import { Helmet, withModulesManager, formatMessage, withTooltip, historyPush, decodeId } from "@openimis/fe-core";
+import { bindActionCreators } from "redux";
+import {
+  Helmet,
+  withModulesManager,
+  formatMessage,
+  withTooltip,
+  historyPush,
+  decodeId,
+  clearCurrentPaginationPage,
+} from "@openimis/fe-core";
 import { injectIntl } from "react-intl";
 import { withTheme, withStyles } from "@material-ui/core/styles";
 import { connect } from "react-redux";
-import { RIGHT_POLICYHOLDERCONTRACT_SEARCH, RIGHT_POLICYHOLDERCONTRACT_CREATE,
-    RIGHT_POLICYHOLDERCONTRACT_UPDATE, RIGHT_POLICYHOLDERCONTRACT_APPROVE } from "../constants"
+import {
+  RIGHT_POLICYHOLDERCONTRACT_SEARCH,
+  RIGHT_POLICYHOLDERCONTRACT_CREATE,
+  RIGHT_POLICYHOLDERCONTRACT_UPDATE,
+  RIGHT_POLICYHOLDERCONTRACT_APPROVE,
+  CONTRACT_ROUTE_CONTRACT,
+  MODULE_NAME,
+} from "../constants";
 import ContractSearcher from "../components/ContractSearcher";
 import { Fab } from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
 
-const styles = theme => ({
-    page: theme.page,
-    fab: theme.fab
-})
-
-class ContractsPage extends Component {
-    onAdd = () => historyPush(this.props.modulesManager, this.props.history, "contract.route.contract");
-
-    contractUpdatePageUrl = contract => `${this.props.modulesManager.getRef("contract.route.contract")}${"/" + decodeId(contract.id)}`;
-
-    onDoubleClick = (contract, newTab = false) => {
-        const { rights, modulesManager, history } = this.props;
-        if (rights.includes(RIGHT_POLICYHOLDERCONTRACT_UPDATE) || rights.includes(RIGHT_POLICYHOLDERCONTRACT_APPROVE)) {
-            historyPush(modulesManager, history, "contract.route.contract", [decodeId(contract.id)], newTab);
-        }
-    }
-    
-    render() {
-        const { intl, classes, rights } = this.props;
-        return (
-            rights.includes(RIGHT_POLICYHOLDERCONTRACT_SEARCH) && (
-                <div className={classes.page}>
-                    <Helmet title={formatMessage(this.props.intl, "contract", "contracts.page.title")} />
-                    <ContractSearcher
-                        onDoubleClick={this.onDoubleClick}
-                        contractUpdatePageUrl={this.contractUpdatePageUrl}
-                        rights={rights}
-                    />
-                    {rights.includes(RIGHT_POLICYHOLDERCONTRACT_CREATE) && withTooltip(
-                        <div className={classes.fab} >
-                            <Fab color="primary" onClick={this.onAdd}>
-                                <AddIcon />
-                            </Fab>
-                        </div>,
-                        formatMessage(intl, "contract", "createButton.tooltip")
-                    )}
-                </div>
-            )
-        )
-    }
-}
-
-const mapStateToProps = state => ({
-    rights: !!state.core && !!state.core.user && !!state.core.user.i_user ? state.core.user.i_user.rights : []
+const styles = (theme) => ({
+  page: theme.page,
+  fab: theme.fab,
 });
 
-export default withModulesManager(injectIntl(withTheme(withStyles(styles)(connect(mapStateToProps, null)(ContractsPage)))));
+class ContractsPage extends Component {
+  onAdd = () =>
+    historyPush(
+      this.props.modulesManager,
+      this.props.history,
+      CONTRACT_ROUTE_CONTRACT
+    );
+
+  contractUpdatePageUrl = (contract) =>
+    `${this.props.modulesManager.getRef(CONTRACT_ROUTE_CONTRACT)}${
+      "/" + decodeId(contract.id)
+    }`;
+
+  onDoubleClick = (contract, newTab = false) => {
+    const { rights, modulesManager, history } = this.props;
+    if (
+      rights.includes(RIGHT_POLICYHOLDERCONTRACT_UPDATE) ||
+      rights.includes(RIGHT_POLICYHOLDERCONTRACT_APPROVE)
+    ) {
+      historyPush(
+        modulesManager,
+        history,
+        CONTRACT_ROUTE_CONTRACT,
+        [decodeId(contract.id)],
+        newTab
+      );
+    }
+  };
+
+  componentDidMount = () => {
+    const { module } = this.props;
+    if (module !== MODULE_NAME) this.props.clearCurrentPaginationPage();
+  };
+
+  render() {
+    const { intl, classes, rights } = this.props;
+    return (
+      rights.includes(RIGHT_POLICYHOLDERCONTRACT_SEARCH) && (
+        <div className={classes.page}>
+          <Helmet
+            title={formatMessage(
+              this.props.intl,
+              "contract",
+              "contracts.page.title"
+            )}
+          />
+          <ContractSearcher
+            onDoubleClick={this.onDoubleClick}
+            contractUpdatePageUrl={this.contractUpdatePageUrl}
+            rights={rights}
+          />
+          {rights.includes(RIGHT_POLICYHOLDERCONTRACT_CREATE) &&
+            withTooltip(
+              <div className={classes.fab}>
+                <Fab color="primary" onClick={this.onAdd}>
+                  <AddIcon />
+                </Fab>
+              </div>,
+              formatMessage(intl, "contract", "createButton.tooltip")
+            )}
+        </div>
+      )
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  rights:
+    !!state.core && !!state.core.user && !!state.core.user.i_user
+      ? state.core.user.i_user.rights
+      : [],
+  module: state.core?.savedPagination?.module,
+});
+
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators({ clearCurrentPaginationPage }, dispatch);
+
+export default withModulesManager(
+  injectIntl(
+    withTheme(
+      withStyles(styles)(
+        connect(mapStateToProps, mapDispatchToProps)(ContractsPage)
+      )
+    )
+  )
+);
